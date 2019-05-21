64fd528a1520ba49dc809a4801558a755aa02418
doruksahin <doruksahin98@gmail.com>
2019-05-21 10:55:43 +0300

    .

---
+++ holyconfessions/routes.py
@@ -0,0 +1,69 @@
+from flask import render_template, url_for, flash, redirect
+from holyconfessions import app, bcrypt
+from holyconfessions.forms import RegistrationForm, LoginForm
+from holyconfessions.models import User, Post
+from flask_login import login_user, current_user, logout_user
+
+posts = [
+	{
+		'author': 'Doruk',
+		'title': 'Olumsuzluk',
+		'date': '22.04.1998'
+	},
+	{
+		'author': 'Doruk',
+		'title': 'Motorsiklet',
+		'date': '20.05.2019'
+	}
+	
+]
+
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+    	hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    	user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+    	db.session.add(user)
+    	db.session.commit()
+        flash('{} adli hesap olusturuldu.'.format(form.username.data), 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Giris basarisiz.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
