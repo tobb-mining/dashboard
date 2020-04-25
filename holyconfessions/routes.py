f436b4bfd3b54387fdef2464f817552123facbc5
doruksahin <doruksahin98@gmail.com>
2020-04-25 16:44:21 +0300

    for User not found error, used db.create_all() and added db to routes.py

--- holyconfessions/routes.py
+++ holyconfessions/routes.py
@@ -3,6 +3,7 @@ from holyconfessions import app, bcrypt
 from holyconfessions.forms import RegistrationForm, LoginForm
 from holyconfessions.models import User, Post
 from flask_login import login_user, current_user, logout_user
+from holyconfessions import db
 
 posts = [
 	{
@@ -37,15 +38,15 @@ def register():
 	if current_user.is_authenticated:
 		return redirect(url_for('home'))
 
-    form = RegistrationForm()
-    if form.validate_on_submit():
-    	hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-    	user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-    	db.session.add(user)
-    	db.session.commit()
-        flash('{} adli hesap olusturuldu.'.format(form.username.data), 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		flash('{} adli hesap olusturuldu.'.format(form.username.data), 'success')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -53,15 +54,15 @@ def login():
 	if current_user.is_authenticated:
 		return redirect(url_for('home'))
 
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
-        else:
-            flash('Giris basarisiz.', 'danger')
-    return render_template('login.html', title='Login', form=form)
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
+			return redirect(url_for('home'))
+		else:
+			flash('Giris basarisiz.', 'danger')
+	return render_template('login.html', title='Login', form=form)
 
 @app.route("/logout")
 def logout():
